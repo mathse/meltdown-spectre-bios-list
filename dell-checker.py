f24434cb61dca684380a60f08e8d1a5a48d2955c
Mathias Decker <mathias@itgweb.de>
2018-03-27 21:58:52 +0200

    updated for easier parsing

--- dell-checker.py
+++ dell-checker.py
@@ -10,20 +10,31 @@ for row in soup.findAll('table')[1].findAll('tbody')[0].findAll('tr'):
 
 
     try:
+    # if 1:
         # t =
         # print name
+        line = os.popen("grep '%s' README.md" % name).read()
+        old_version = line.split(" | ")[1].replace(" ","").replace("\n","")
+        try:
+            status = line.split(" | ")[3].split(" ")[0]
+        except:
+            status = "no"
+        # print status
+        if status == "no":
+            if old_version != new_version and new_version != "In Process":
+                # print old_version
+                new_version_link = row.findAll('td')[1].findAll('a')[0]
 
-        old_version = os.popen("grep '%s' README.md" % name).read().split(" | ")[1].replace(" ","").replace("\n","")
-        if old_version != new_version and new_version != "In Process":
-            # print old_version
-            new_version_link = row.findAll('td')[1].findAll('a')[0]
-
-            page_version = urllib2.urlopen('http:%s' % new_version_link['href']).read()
-            soup_version = BeautifulSoup(page_version)
-            soup_version.prettify()
-            # print soup_version.findAll('div', class_="default top-offset-5")
-            new_version_date = soup_version.findAll('div')[32].text
-            # new_version_link['href']
-            print "%s \t %s -> %s | %s" % (name,old_version,new_version,new_version_date)
+                page_version = urllib2.urlopen('http:%s' % new_version_link['href']).read()
+                soup_version = BeautifulSoup(page_version)
+                soup_version.prettify()
+                # print soup_version.findAll('div', class_="default top-offset-5")
+                if soup_version.findAll('div')[32].text == 'WichtigkeitDringend':
+                    new_version_date = soup_version.findAll('div')[31].text
+                else:
+                    new_version_date = soup_version.findAll('div')[32].text
+                
+                # new_version_link['href']
+                print "%s \t %s -> %s | %s | yes (not verified)" % (name,old_version,new_version,new_version_date)
     except:
         print "%s is missing" % name
